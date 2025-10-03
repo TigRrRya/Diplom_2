d684651d13d4624c0bb150990bb9a751e94baa97
Aleksandr Shorin <Aleksandr.Sh.S@yandex.ru>
2025-10-03 23:26:28 +0500

    add allure report

---
+++ src/main/java/stellarburgers/api/ConstantsApiAndUrl.java
@@ -0,0 +1,12 @@
+package stellarburgers.api;
+
+public class ConstantsApiAndUrl {
+    public static final String BASE_URL = "https://stellarburgers.nomoreparties.site";
+    public static final String REGISTER_USER = "/api/auth/register";
+    public static final String LOGIN_USER = "/api/auth/login";
+    public static final String CREATE_ORDERS = "/api/orders";
+    public static final String DATE_INGREDIENTS = "/api/ingredients";
+    public static final String DELETE_USER = "/api/auth/user";
+
+
+}
